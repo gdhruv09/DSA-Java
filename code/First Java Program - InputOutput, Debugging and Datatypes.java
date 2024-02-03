c9f0f49d3d08c826b807e16cb3bc8fee7f44448c
gdhruv09 <156504589+gdhruv09@users.noreply.github.com>
2024-02-04 05:10:57 +0530

    code file modified.

--- code/First Java Program - InputOutput, Debugging and Datatypes.java
+++ code/First Java Program - InputOutput, Debugging and Datatypes.java
@@ -55,3 +55,133 @@ class Main {
     System.out.println("the temperature in F is- " + f);
   }
 }
+
+
+## Write Java programs for the following:
+
+1. Write a program to print whether a number is even or odd, also take input from the user.
+
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter a number: ");
+    int number = input.nextInt();
+
+    if (number % 2 == 0) {
+      System.out.println("The number is even");
+    } else {
+      System.out.println("The number is odd");
+    }
+  }
+}
+
+
+2. Take name as input and print a greeting message for that particular name.
+
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    System.out.print("Enter your name: ");
+    String name = in.nextLine();
+    System.out.println("Hello " + name + "!");
+  }
+}
+
+3. Write a program to input principal, time, and rate (P, T, R) from the user and find Simple Interest.
+
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    System.out.print("Enter Priciple: ");
+    double p = in.nextDouble();
+    System.out.print("Enter Interest Rate: ");
+    double r = in.nextDouble();
+    System.out.print("Enter Number of Months: ");
+    int n = in.nextInt();
+
+    double SI = p * r * n / 100;
+    System.out.println("The SI is: " + SI);
+  }
+}
+
+4. Take in two numbers and an operator (+, -, *, /) and calculate the value. (Use if conditions)
+
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter first number: ");
+    int num1 = input.nextInt();
+    System.out.print("Enter second number: ");
+    int num2 = input.nextInt();
+    System.out.print("Enter operator: ");
+    char op = input.next().charAt(0);
+    int result = 0;
+
+    if (op == '+') {
+      result = num1 + num2;
+    } else if (op == '-') {
+      result = num1 - num2;
+    } else if (op == '*') {
+      result = num1 * num2;
+    } else if (op == '/') {
+      result = num1 / num2;
+    }
+    System.out.println("the result is: " + result);
+  }
+}
+
+
+5. Take 2 numbers as input and print the largest number.
+
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    System.out.print("Enter first number: ");
+    int n = in.nextInt();
+    System.out.print("Enter second number: ");
+    int m = in.nextInt();
+
+    if (n > m) {
+      System.out.print("the bigger number is :" + n);
+    } else
+      System.out.print("the bigger number is : " + m);
+  }
+}
+
+6. Input currency in rupees and output in USD.
+
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    System.out.print("Enter the amount: ");
+    double inr = in.nextDouble();
+
+    double usd = inr / 74.45;
+
+    System.out.println("The amount in USD is: " + usd);
+
+  }
+}
+
+
+7. To calculate Fibonacci Series up to n numbers.
+
+
+
+
+8. To find out whether the given String is Palindrome or not.
+
+
+9. To find Armstrong Number between two given number.
---
+++ code/Flow Of Program.java
@@ -0,0 +1,52 @@
+## Create pseudocode for the following:
+
+1. Input a year and find whether it is a leap year or not.
+
+Start 
+Input year
+If (year % 4 == 0 && year % 100 ! = 0 || year % 400 = 0)
+output "Leap Year"
+exit
+else output "Not Leap Year"
+Exit 
+
+2. Take two numbers and print the sum of both.
+
+Start
+Input Number1 
+Input Number2
+sum = Number1 + Number2
+Output (sum)
+exit
+
+3. Take a number as input and print the multiplication table for it.
+
+start 
+input number 
+for (int i=0; i<=10; i++>)
+output (number)
+Stop
+
+4. Take 2 numbers as inputs and find their HCF and LCM.
+
+start
+input number1
+input number2 
+While (HCF>1)
+HCF = number1 % number2
+output (HCF)
+end while
+Lcm = num1*num2/hcf
+Output (LCM)
+Stop
+
+5. Keep taking numbers as inputs till the user enters ‘x’, after that print sum of all.
+
+start
+input number
+while (input=x)
+if input == x;
+break
+else sum = sum + number
+output "sum"
+Stop
